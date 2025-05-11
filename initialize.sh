3092eeab1b7bd67b9a3bcf686c717edbe32b1220
Iain Samuel McLean Elder <iain@isme.es>
2025-05-11 02:23:03 +0200

    Set the distribution to sign the changes file

--- initialize.sh
+++ initialize.sh
@@ -14,5 +14,10 @@ rm -rf aws-gate/
 # Make a debian metadata folder.
 dh_make --python --createorig --packagename aws-gate_0.11.3 --yes
 
+# Set the distribution so that debuild signs the changes file.
+# At best dput will fail loudly. At worst it appears to work but Launchpad never gives email feedback.
+# https://help.launchpad.net/Packaging/UploadErrors
+sed -i 's/UNRELEASED/noble/' debian/changelog
+
 # Install the build dependencies.
 sudo apt install build-essential:native dh-sequence-python3 python3-all
